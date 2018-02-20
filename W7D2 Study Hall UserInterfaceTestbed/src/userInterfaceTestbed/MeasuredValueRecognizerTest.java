3fac6165c72054513b6094d92b3ac601613da124
Sukrut Patil <34018683+SukrutPatil@users.noreply.github.com>
2018-02-20 22:26:56 +0530

    Add files via upload

---
+++ W7D2 Study Hall UserInterfaceTestbed/src/userInterfaceTestbed/MeasuredValueRecognizerTest.java
@@ -0,0 +1,14 @@
+package userInterfaceTestbed;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MeasuredValueRecognizerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
